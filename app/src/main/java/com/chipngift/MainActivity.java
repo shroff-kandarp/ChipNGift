d8a1452aa639c611865e092a173bcb7d143ae60a
Ravi Shukla <ravi.ushukla@gmail.com>
2016-12-08 01:47:20 +0530

    check signin screen

--- app/src/main/java/com/chipngift/MainActivity.java
+++ app/src/main/java/com/chipngift/MainActivity.java
@@ -1,25 +1,55 @@
 package com.chipngift;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_signup, btn_login;
+    TextView tv_skip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("Fine","SetUp");
-        Log.d("Fine","SetUp");
-        Log.d("Fine","SetUp");
-        Log.d("Fine","SetUp");
-        Log.d("Fine","SetUp");
+        btn_signup = (Button) findViewById(R.id.btn_signup);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        tv_skip = (TextView) findViewById(R.id.tv_skip);
+
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(i);
+            }
+        });
 
 
 
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+        tv_skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }

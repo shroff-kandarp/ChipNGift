414362be2bb60328b11386d6f878953d39907192
Ravi Shukla <ravi.ushukla@gmail.com>
2016-12-08 01:38:19 +0530

    check signin screen

---
+++ app/src/main/java/com/chipngift/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.chipngift;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ravi on 08-12-2016.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
+}
